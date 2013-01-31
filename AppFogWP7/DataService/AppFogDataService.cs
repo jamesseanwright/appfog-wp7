2fad6973915d11c551c161272d36afdbfe716f5b
James Wright <james.wright@bskyb.com>
2013-01-31 09:33:59 -0800

    Moved api stuff to separate class. currently working on async stuff

---
+++ AppFogWP7/DataService/AppFogDataService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+using System.Runtime.Serialization;
+using GalaSoft.MvvmLight;
+using AppFogWP7.Model;
+using System.Net;
+using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json.Linq;
+
+namespace AppFogWP7.DataService
+{
+    public class AppFogDataService
+    {
+        public void CallAPI(string token, Action<InfoModel, Exception> callback)
+        {
+            WebClient client = new WebClient();
+            client.Headers[HttpRequestHeader.Authorization] = token;
+
+            client.DownloadStringCompleted += (sender, args) =>
+            {
+                InfoModel newModel = new InfoModel();
+
+                JObject infoJson = JObject.Parse(args.Result);
+                newModel.User = infoJson["user"].ToString();
+                newModel.Plan = infoJson["plan"].ToString();
+
+                foreach (JToken framework in infoJson["frameworks"].Children())
+                {
+                    newModel.Frameworks.Add(framework.First["name"].ToString());
+                }
+
+                newModel.Apps = (int) infoJson["usage"]["apps"];
+                newModel.MemoryUsed = (int) infoJson["usage"]["memory"];
+                newModel.TotalMemory = (int) infoJson["limits"]["memory"];
+                
+                callback(newModel, null);
+            };
+
+            client.DownloadStringAsync(new Uri("https://api.appfog.com/info"));
+        }
+    }
+}
--- AppFogWP7/ViewModel/MainViewModel.cs
+++ AppFogWP7/ViewModel/MainViewModel.cs
@@ -9,51 +9,15 @@ using AppFogWP7.Model;
 using System.Net;
 using GalaSoft.MvvmLight.Command;
 using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
+using AppFogWP7.DataService;
 
 namespace AppFogWP7.ViewModel
 {
-    /// <summary>
-    /// This class contains properties that the main View can data bind to.
-    /// <para>
-    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
-    /// </para>
-    /// <para>
-    /// You can also use Blend to data bind with the tool's support.
-    /// </para>
-    /// <para>
-    /// See http://www.galasoft.ch/mvvm
-    /// </para>
-    /// </summary>
-    public class MainViewModel : ViewModelBase
+    public class MyAppViewModelBase : ViewModelBase
     {
-        /// <summary>
-        /// Initializes a new instance of the MainViewModel class.
-        /// </summary>
-        public MainViewModel()
-        {
-            _infoModel = new InfoModel();
-            _loading = false;
-            _loaded = false;
-            _getInfoCommand = new RelayCommand<string>(GetInfo);
-        }
+        private bool _loading;
+        private bool _loaded;
 
-        #region properties
-
-        public InfoModel InfoModel
-        {
-            get { return _infoModel; }
-            set
-            {
-                _infoModel = value;
-                RaisePropertyChanged("InfoModel");
-            }
-        }
-
-        public RelayCommand<string> GetInfoCommand
-        {
-            get { return _getInfoCommand; }
-        }
 
         public bool Loading
         {
@@ -74,40 +38,64 @@ namespace AppFogWP7.ViewModel
                 RaisePropertyChanged("Loaded");
             }
         }
+   
+    }
 
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : MyAppViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            _infoModel = new InfoModel();
+            _getInfoCommand = new RelayCommand<string>(GetInfo);
+        }
+
+        #region properties
+
+        public InfoModel InfoModel
+        {
+            get { return _infoModel; }
+            set
+            {
+                _infoModel = value;
+                RaisePropertyChanged("InfoModel");
+            }
+        }
+
+        public bool IsInfoModelAvailble
+        {
+            get { return InfoModel == null; }
+        }
+
+        public RelayCommand<string> GetInfoCommand
+        {
+            get { return _getInfoCommand; }
+        }
+
+ 
         #endregion
 
         #region methods
         public void GetInfo(string token)
         {
             Loading = true;
-            WebClient client = new WebClient();
-            client.Headers[HttpRequestHeader.Authorization] = token;
-            client.DownloadStringCompleted += new DownloadStringCompletedEventHandler(GetInfoDownloadStringCompleted);
-            client.DownloadStringAsync(new Uri("https://api.appfog.com/info"));
-        }
-        public void GetInfoDownloadStringCompleted(object sender, DownloadStringCompletedEventArgs args)
-        {
-            InfoModel newModel = new InfoModel();
-
-            JObject infoJson = JObject.Parse(args.Result);
-            newModel.User = infoJson["user"].ToString();
-            newModel.Plan = infoJson["plan"].ToString();
-
-            foreach(JToken framework in infoJson["frameworks"].Children())
-            {
-                newModel.Frameworks.Add(framework.First["name"].ToString());
-            }
-
-            newModel.Apps = (int) infoJson["usage"]["apps"];
-            newModel.MemoryUsed = (int) infoJson["usage"]["memory"];
-            newModel.TotalMemory = (int) infoJson["limits"]["memory"];
-
-
-
-            InfoModel = newModel;
+            AppFogDataService appFogDataService = new AppFogDataService();
+            appFogDataService.CallAPI(token, (newItem, e) => InfoModel = newItem);
             Loading = false;
-            Loaded = true;
         }
 
         #endregion
@@ -117,8 +105,7 @@ namespace AppFogWP7.ViewModel
         private InfoModel _infoModel;
         private RelayCommand<string> _getInfoCommand;
         private string _appFogToken;
-        private bool _loading;
-        private bool _loaded;
+        
 
         #endregion
 
